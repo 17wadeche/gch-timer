4f3733a86813e43b6760f390a3e5180a09adb768
17wadeche <78323562+17wadeche@users.noreply.github.com>
2026-01-28 12:57:43 -0600

    d

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -491,7 +491,7 @@ if not wkdf.empty:
         return "PLI Level"
     wkdf.loc[:, "bucket"] = wkdf.apply(lambda r: map_bucket(r["section"], r["source"]), axis=1)
     palette_domain = ["Reportability","Regulatory Report","Regulatory Inquiry",
-                  "Product Analysis","Investigation","Communication","Task","PLI Level","CW"]
+                  "Product Analysis","Investigation","Communication","Task","PLI Level","Complaint Wizard"]
     palette_range  = ["#ff7f0e","#1f77b4","#2ca02c",
                     "#9467bd","#d62728","#8c564b","#e377c2","#7f7f7f","#17becf"]
     axis = alt.Axis(
